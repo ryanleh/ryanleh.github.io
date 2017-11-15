ebbf4e5dca06bb5fdde80943990f1a7ae2e268a0
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2017-11-14 18:10:16 -0800

    add js

--- js/toggle.js
+++ js/toggle.js
@@ -1,20 +1,40 @@
 
+
 function SlideMainbarIn() {
     var mainbar = document.getElementById("mainbar");
     var maintext = document.getElementById("maintext");
     var about_me = document.getElementById("about_me");
-	mainbar.style.width = "30%";
-    maintext.style.visibility = "visible";
-    about_me.style.visibility = "hidden";
 
+    maintext.style.transition = "opacity 3s ease";
+    about_me.style.transition = "opacity .75s ease";
+
+	mainbar.style.width = "30%";
+    about_me.style.opacity = "0";
+    setTimeout(function() 
+        { 
+            about_me.style.visibility = "hidden";  
+        }, 500);
+    maintext.style.visibility = "visible";
+    maintext.style.opacity = "1";
+    
 }
 
 function SlideMainbarOut() {
     var mainbar = document.getElementById("mainbar");
     var maintext = document.getElementById("maintext");
     var about_me = document.getElementById("about_me");
+
+    maintext.style.transition = "opacity .5s ease";
+    about_me.style.transition = "opacity 4s ease";
+
     mainbar.style.width = "50%";
-    maintext.style.visibility = "hidden";
+    maintext.style.opacity = "0";
+    setTimeout(function() 
+        { 
+            maintext.style.visibility = "hidden";  
+        }, 500);
     about_me.style.visibility = "visible";
-    
+    about_me.style.opacity = "1";
 }
+
+
