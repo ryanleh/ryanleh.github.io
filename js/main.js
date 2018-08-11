35a56a370eeeea20060d2b6ce86cb890911ddb7e
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-08-10 20:48:57 -0700

    Update for mobile

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
+  
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
@@ -26,25 +13,21 @@
     offset: 56
   });
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  $('.close-modal').click(function(e) {
+    $('.member-modal').modal('hide');
+    $(document.body).addClass('test');
+  });
 
-  // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
-    $(".navbar").addClass("d-none");
-  })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
-    $(".navbar").removeClass("d-none");
-  })
+
+  $(function () {
+    $('[data-bm-close][data-bm-open]').on('click', function () {
+      var $this = $(this);
+      $($this.data('bm-close')).one('hidden.bs.modal', function () {
+          $($this.data('bm-open')).modal('show');
+      }).modal('hide');
+    });
+  });
 
 })(jQuery); // End of use strict
+
+
